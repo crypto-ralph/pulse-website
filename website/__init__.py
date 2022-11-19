e80c68bffa9419c45055caef88b1e84eeae68c96
CryptoRalph <doxter@protonmail.com>
2022-11-19 20:17:58 +0100

    Preparation for deployment

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,14 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['TESTING'] = True
-    app.config['SECRET_KEY'] = 'supersecret'
-
-    # Blueprints register
-    # app.register_blueprint(auth)
+    app.config['TESTING'] = False if os.environ.get('APP_ENV') == 'PROD' else True
+    app.config['DEBUG'] = False if os.environ.get('APP_ENV') == 'PROD' else True
+    app.config['SECRET_KEY'] = os.environ.get("APP_SECRET_KEY")
 
     CORS(app)
     return app
